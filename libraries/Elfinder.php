4c04beb2bd7163e1ef9a8e68ed2982e9210951bf
EmmaMKB <emmanuelmakabu7@gmail.com>
2018-08-27 09:17:39 -0400

    [add] add get a set methods for the site folders

--- libraries/Elfinder.php
+++ libraries/Elfinder.php
@@ -1,8 +1,37 @@
-<?php 
+<?php
+
+    namespace clearos\apps\webfile_manager;
 class Elfinder
 {
+    static $site_folder = array();
+
     public function __construct()
     {
-        
-    }   
+
+    }
+
+    /**
+     * @param array $site_folder
+     */
+    public static function setSiteFolder()
+    {
+
+        $site_folder_path = "/var/www/virtual/";
+        $site_folders = array();
+
+        $folder = opendir($site_folder_path);
+        while($item = readdir($folder)) {
+            if(is_dir($site_folder_path.$item) && ($item != ".") && ($item != ".."))
+                $site_folders[] = $item;
+        }
+        self::$site_folder = $site_folders;
+    }
+
+    /**
+     * @return array
+     */
+    public static function getSiteFolder()
+    {
+        return self::$site_folder;
+    }
 }
