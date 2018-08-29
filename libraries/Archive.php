d4094770c0479f2aaf082716bb1153533e9fed1e
jonathanyombo@gmail.com <jonathanyombo@gmail.com>
2018-08-29 11:07:53 +0200

    [ADD] a constructor to archive class

--- libraries/Archive.php
+++ libraries/Archive.php
@@ -1,8 +1,12 @@
 <?php
 
 namespace clearos\apps\webfile_manager;
-
-class Archive
+require_once "vendor/autoload.php";
+use \wapmorgan\UnifiedArchive\UnifiedArchive as UnifiedArchive;
+class Archive extends UnifiedArchive
 {
-    
+    function __construct($fileName, $type)
+    {
+        parent::__construct($fileName, $type);
+    }
 }
