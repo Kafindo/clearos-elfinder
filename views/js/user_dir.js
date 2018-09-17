238095ee5831f48edaab631e8771f37308704b95
Bush <keevbush@gmail.com>
2018-09-17 15:42:11 +0200

    [UPDATE] load icons with type of file

--- views/js/user_dir.js
+++ views/js/user_dir.js
@@ -3,18 +3,19 @@ var jqxhr = $.get( "/app/webfile_manager/execute/getUser_dirJson", function() {
   })
     .done(function(data) {
       console.log(data);
-
-      data=data.replace(/"/g,'').replace('[','').replace(']','');
+      data=data.replace(/[[]/g,'').replace(/]/g,'').replace(/"/g,'');
       var folder_files=data.split(",");
-      console.log(folder_files);
-      var mainFraime=document.getElementById("main-content");
-      folder_files.forEach(element => {
-       // console.log(element);
-       var child= document.createElement("a");
+      var final_folder_file;
+      alert(folder_files[1]);
+      var mainFrame=document.getElementById("main-content");
+      for(var i =0;i<folder_files.length;i=i+2){
+        var child= document.createElement("a");
         child.className="col-md-1 col-lg-1 col-sm-2 col-xs-2";
-        child.innerHTML=element;
-        mainFraime.appendChild(child);
-      });
+        var img=getExtImg(folder_files[i],folder_files[i+1]);
+        child.innerHTML=img+folder_files[i];      
+        mainFrame.appendChild(child);
+      }
+     
     })
     .fail(function(eror,e1,e2) {
       console.log(e2);
