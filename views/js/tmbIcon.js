3a37aee391c2fae6ab6af509ca9fd3939020019a
Jonathan YOMBO <jonathanyombo@gmail.com>
2018-09-17 13:06:57 +0200

    [UPDATE] modifie size of icon

--- views/js/tmbIcon.js
+++ views/js/tmbIcon.js
@@ -1,4 +1,4 @@
 function getExtImg(filename) {
     var ext = filename.split('.').pop();
-    return "<img src='" + document.location.href + "assets/img/" + ext + ".png' alt ='"+ ext +"' class='img-responsive'>";
+    return "<img src='" + document.location.href + "assets/img/" + ext + ".png' alt ='"+ ext +"' class='img-responsive' width='40px'>";
 }
